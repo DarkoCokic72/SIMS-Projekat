7011f197b34e402d073ba8a50f9510217ae9e395
Sima Krestic <krestic.ra78.2019@uns.ac.rs>
2022-04-29 10:31:36 +0200

    implemented login function

---
+++ HospitalApp/WpfApp1/Repo/ManagerRepository.cs
@@ -0,0 +1,27 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.FileHandler;
+using WpfApp1.Model;
+
+namespace WpfApp1.Repo
+{
+    public class ManagerRepository
+    {
+        public List<Manager> GetAll()
+        {
+            return managerFileHandler.Read();
+        }
+
+        public FileHandler.ManagerFileHandler managerFileHandler;
+
+        public ManagerRepository(ManagerFileHandler fileHandler)
+        {
+            managerFileHandler = fileHandler;
+        }
+
+    }
+}
---
+++ HospitalApp/WpfApp1/Service/UserAccountService.cs
@@ -0,0 +1,31 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Repo;
+
+namespace WpfApp1.Service
+{
+    public class UserAccountService
+    {
+        public List<UserAccount> GetAll()
+        {
+            return userAccountRepository.GetAll();
+        }
+
+        public UserAccount GetByEmailPassword(string email, string password)
+        {
+            return userAccountRepository.GetByEmailPassword(email, password);
+        }
+
+        public Repo.UserAccountRepository userAccountRepository;
+
+        public UserAccountService(UserAccountRepository userAccountRepository)
+        {
+            this.userAccountRepository = userAccountRepository;
+        }
+
+    }
+}
