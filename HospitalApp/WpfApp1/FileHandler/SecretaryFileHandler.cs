3fc4df5f0757d1f13ecc1d82f0023d3e2a91f556
Sima Krestic <krestic.ra78.2019@uns.ac.rs>
2022-05-02 14:03:06 +0200

    login for Managers

--- HospitalApp/WpfApp1/FileHandler/SecretaryFileHandler.cs
+++ HospitalApp/WpfApp1/FileHandler/SecretaryFileHandler.cs
@@ -4,24 +4,40 @@
  * Purpose: Definition of the Class FileHandler.SecretaryFileHandler
  ***********************************************************************/
 
+using FileHandler;
+using Model;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
 
-namespace FileHandler
+namespace WpfApp1.FileHandler
+
 {
     public class SecretaryFileHandler
-   {
-      private string path;
-      
-      public List<Secretary> read()
-      {
-         throw new NotImplementedException();
-      }
-      
-      public void write(ref List<Secretary> secretary)
-      {
-         throw new NotImplementedException();
-      }
-   
-   }
+{
+
+    public List<Secretary> Read()
+    {
+
+        if (!System.IO.File.Exists(path))
+        {
+            return new List<Secretary>();
+        }
+
+        string secretarySerialized = System.IO.File.ReadAllText(path);
+        List<Secretary> secretary = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Secretary>>(secretarySerialized);
+        return secretary;
+    }
+
+    public void Save(List<Secretary> secretary)
+    {
+        string secretarySerialized = Newtonsoft.Json.JsonConvert.SerializeObject(secretary);
+        System.IO.File.WriteAllText(path, secretarySerialized);
+    }
+
+    private string path = @"..\..\Data\Secretaries.txt";
+}
 }
