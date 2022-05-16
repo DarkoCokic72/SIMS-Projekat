48cb2ab2f262e255e34a97a6de9d12da739b34e6
Sima Krestic <krestic.ra78.2019@uns.ac.rs>
2022-05-16 03:30:31 +0200

    create urgent case implemented

---
+++ HospitalApp/WpfApp1/Model/PhysicianSpecialization.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Model
+{
+    public enum PhysicianSpecialization
+    {
+        surgeon,
+        pulmonologist,
+        physiotherapist,
+        survival_practice
+    }
+}
